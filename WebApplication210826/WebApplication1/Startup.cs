cb370174b553c8640c9e439d523c889f0e1792e6
sz_net_xiexing <49037007@qq.com>
2021-09-15 11:52:08 +0800

    新加swagger配置

--- WebApplication210826/WebApplication1/Startup.cs
+++ WebApplication210826/WebApplication1/Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 namespace WebApplication1
 {
@@ -25,6 +27,25 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //注册Swagger生成器，定义一个和多个Swagger 文档
+            services.AddSwaggerGen(c =>
+            {
+               
+
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "系统 API", Version = "v1" });
+              
+
+                // 接口排序
+                c.OrderActionsBy(o => o.GroupName + "," + o.HttpMethod + "," + o.RelativePath);
+                //// 配置 xml 文档
+                //var xmlFileLoacl = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPathLoacl = System.IO.Path.Combine(BaseDirectory, xmlFileLoacl);
+                //c.IncludeXmlComments(xmlPathLoacl, true);
+
+
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            });
+
             services.AddControllers();
         }
 
@@ -42,10 +63,22 @@ namespace WebApplication1
 
             app.UseAuthorization();
 
+             #region Swagger           
+            //启用Swagger
+            app.UseSwagger().UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "系统 API V1");
+                // 访问Swagger的路由后缀
+                c.RoutePrefix = "swagger";
+            });
+            #endregion
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            
         }
     }
 }
